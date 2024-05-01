26617fa6f8509040cb292b821b53e9bd70428203
Lenny <166391256+lennytheracoon@users.noreply.github.com>
2024-05-01 11:06:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
-import cv2
 from ultralytics import YOLO  # Assuming ultralytics is installed
 
 # Install Plotly if not already installed
